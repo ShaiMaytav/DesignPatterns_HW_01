8e59a23932ae9c8050b24ecf2e9d62135790e892
ShaiMaytav <87770707+ShaiMaytav@users.noreply.github.com>
2021-11-13 17:18:39 +0200

    Add files via upload

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Physics physics = Physics.Instance;
+            Console.WriteLine(physics.Gravity);
+            Console.WriteLine(physics.CheckCollision());
+        }
+    }
+
+    public class Physics
+    {
+
+        private static Physics instance;
+        private Physics() { }
+
+        public static Physics Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Physics();
+                }
+                return instance;
+            }
+        }
+
+        public float Gravity = 9.807f;
+
+        public bool CheckCollision()
+        {
+            if (true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
